901ee1eb065ac9091d590891652fe3cfb6c11d02
rudub <35490403+rudub@users.noreply.github.com>
2021-03-09 19:43:44 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_db_instance" "msdbinstance" {
+    identifier = var.db_identifier
+    instance_class = var.db_instance_class
+    allocated_storage = var.db_allocated_storage
+    engine_version = var.db_engine_version
+    engine = var.db_engine
+    username = var.db_username
+    password = var.db_password
+    db_subnet_group_name = var.db_subnet_group_name
+}

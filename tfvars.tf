7407af74f226e80f92c707de0117722fb5a15764
rudub <35490403+rudub@users.noreply.github.com>
2021-03-09 20:15:39 +0530

    Create tfvars.tf

---
+++ tfvars.tf
@@ -0,0 +1,17 @@
+aws_default_region = "AWSREGIONS"
+
+db_identifier = "DBNAME"
+
+db_instance_class = "DBINSTANCETYPE"
+
+db_allocated_storage = "DBSTORAGE"
+
+db_engine_version = "DBVERSION"
+
+db_engine = "DBENGINE"
+
+db_username = "DBUSERNAME"
+
+db_password = "DBPASSWORD"
+
+db_subnet_group_name = "DBSUBNETGROUP"
